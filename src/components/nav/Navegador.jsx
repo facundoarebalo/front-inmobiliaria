e64af1553598aac85dac3e90b3e83f00987b33e0
Facundo Arebalo <facuar1589@gmail.com>
2024-11-05 12:15:19 -0300

    navbar e imagenes

--- src/components/nav/Navegador.jsx
+++ src/components/nav/Navegador.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
-
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import logo from '../../assets/img/logo.jpg'
+import { useNavigate } from 'react-router-dom'
+import './navStyle.css'
 const Navegador = () => {
+    const navigate = useNavigate()
+
     return (
-        <div>
-            <nav>
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul>
-            </nav>
-        </div>
+        <Navbar expand="lg" className="bg-body-tertiary shadow-sm">
+            <Container>
+                <Navbar.Brand className='logo' onClick={() => navigate("/")}>
+                    <img
+                        src={logo}
+                        alt="Logo"
+                        className="d-inline-block align-top"
+                        width="30"
+                        height="30"
+                    />{' '}
+                    Spaces Inmobiliaria
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav d-flex">
+                    <Nav className="me-auto">
+                        <Nav.Link className='mx-md-4 text-center' onClick={() => navigate("*")}>Alquileres</Nav.Link>
+                        <Nav.Link className='mx-md-4 text-center' onClick={() => navigate("/contacto")} >Contacto</Nav.Link>
+                        <Nav.Link className='mx-md-4 text-center' onClick={() => navigate("/about")}>Sobre Nosotros</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
 
