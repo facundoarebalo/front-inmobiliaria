8e2f6d31074d35df269324a14d4f20225294ca54
Facundo Arebalo <facuar1589@gmail.com>
2024-11-08 18:28:46 -0300

    agrego banner en view home, agrego favicon y título

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import imgBanner from '../../assets/img/banner-inmobiliaria.jpg'
 
 const Home = () => {
     return (
-        <div>
-            Home
-        </div>
+        <>
+            <div className="container w-100 ">
+             
+                <h1>Bienvenido a nuestra Inmobiliaria</h1>
+                
+                <img src={imgBanner} alt="Banner" className="img-fluid" />
+
+            </div>
+        </>
     )
 }
 
 export default Home
-Home
