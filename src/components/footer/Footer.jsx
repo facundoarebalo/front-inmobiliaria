04486a7624c7e3522f40cf3c9109171eb4ef33ec
Facundo Arebalo <facuar1589@gmail.com>
2024-11-19 11:39:18 -0300

    logo en footer, correción en texto y margenes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,19 +1,19 @@
 import { FaInstagram, FaFacebook, FaTwitter, FaLinkedin } from "react-icons/fa"
+import logo from '../../assets/img/logo.jpg'
 
 const Footer = () => {
     return (
         <>
             <div className=" bg-body-tertiary row shadow-sm mt-4">
-                <div className="col-md-4">
-                    <img src="logo.png" alt="Logo" className="img-fluid" />
-                </div>
-                <div className="col-md-4">
-                    <h4>Ubicación</h4>
-                    <p>Calle 123, Casa 456</p>
-                    <p>Teléfono: 1234567890</p>
-                    <p>Email: info@example.com</p>
-                </div>
                 <div className="col-md-4 d-flex justify-content-center">
+                    <img src={logo} alt="Logo" className="img-fluid w-25" />
+                </div>
+                <div className="col-md-4 text-center">
+                    <p>Calle 123, Oficina 456</p>
+                    <p>Teléfono: 1234567890</p>
+                    <p>Email: spacesinmobiliaria@business.com</p>
+                </div>
+                <div className="col-md-4 d-flex justify-content-center my-4">
                     <FaInstagram className="mx-2" size={30} />
                     <FaFacebook className="mx-2" size={30} />
                     <FaTwitter className="mx-2" size={30} />
